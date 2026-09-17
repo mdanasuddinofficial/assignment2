1c6e9960b4871017ad3967c906b9cbd7fd5a49a2
mdanasuddinofficial <rrabby74@gmail.com>
2026-09-17 16:06:57 +0600

    update code

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -70,17 +70,6 @@ const Navbar = () => {
         <ul className="menu menu-horizontal px-1 text-base font-medium gap-2">
           <li>
             <NavLink
-              to="/"
-              end
-              className={({ isActive }) =>
-                isActive ? "bg-white text-black font-semibold" : ""
-              }
-            >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
               to="/movies"
               className={({ isActive }) =>
                 isActive ? "bg-white text-black font-semibold" : ""
