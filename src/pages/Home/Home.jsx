67d6f3f680f4843a08f99c69cc27081a32baab31
mdanasuddinofficial <rrabby74@gmail.com>
2026-09-17 16:12:24 +0600

    update code

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,12 @@
 import Navbar from "../../components/Navbar/Navbar";
 import Hero from "../../components/Hero/Hero";
 import Footer from "../../components/Footer/Footer";
+import { useEffect } from 'react';
 
 const Home = () => {
+    useEffect(() => {
+    document.title = "Movie Explorer | Home";
+  }, []);
   return (
     <div>
       <Navbar />
