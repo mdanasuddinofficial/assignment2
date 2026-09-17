510174b2f65f51e2c97e25bedff07c09f9f944fa
mdanasuddinofficial <rrabby74@gmail.com>
2026-09-17 16:02:13 +0600

    update code

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
 
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-50 p-2 shadow bg-base-100 rounded-box w-52"
+            className="menu menu-sm dropdown-content mt-3 z-50 p-2 shadow bg-base-100 rounded-box w-52 gap-1"
           >
             <li>
               <NavLink
